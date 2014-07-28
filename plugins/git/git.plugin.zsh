026ec021391305a30304e76f2323fdfef8ad0e0f
aaccurso <accurso.alan@gmail.com>
2014-07-28 09:42:24 -0300

    feat(git): adds git reset --soft alias

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -77,6 +77,7 @@ compdef _git ga=git-add
 alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
+alias grhs='git reset --soft HEAD^'
 alias grhh='git reset HEAD --hard'
 alias gclean='git reset --hard && git clean -dfx'
 alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
